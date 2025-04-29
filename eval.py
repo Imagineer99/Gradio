b51a001f416bb96575a085214cde3936bb8cc6b3
Sam Jackson <samorconor@gmail.com>
2025-04-29 20:42:12 +0100

    style: dynamic svg margins in eval

--- eval.py
+++ eval.py
@@ -77,7 +77,7 @@ def create_evaluate_interface():
                     placeholder="Type your message here...",
                     show_label=False,
                     container=False,
-                    elem_classes=["chat-input", "modern-input", "chat-input-initial"]
+                    elem_classes=["chat-input","eval-input", "modern-input", "chat-input-initial"]
                 )
             with gr.Column(scale=1, elem_classes=["chat-controls-container"]):
                 with gr.Row(elem_classes=["chat-controls"]):
@@ -86,21 +86,21 @@ def create_evaluate_interface():
                         show_label=False, container=False, visible=False
                     )
                     upload_html = """
-                    <div class="upload-svg-wrapper initial-state" title="Upload Image" onclick="this.closest('.chat-controls').querySelector('input[type=\\'file\\']').click()">
+                    <div class="upload-svg-wrapper eval initial-state" title="Upload Image" onclick="this.closest('.chat-controls').querySelector('input[type=\\'file\\']').click()">
                         <svg width="100%" height="100%" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M21 15V16.2C21 17.8802 21 18.7202 20.673 19.362C20.3854 19.9265 19.9265 20.3854 19.362 20.673C18.7202 21 17.8802 21 16.2 21H7.8C6.11984 21 5.27976 21 4.63803 20.673C4.07354 20.3854 3.6146 19.9265 3.32698 19.362C3 18.7202 3 17.8802 3 16.2V15M17 8L12 3M12 3L7 8M12 3V15" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                         </svg>
                     </div>
                     """
                     send_html = """
-                    <div class="send-svg-wrapper initial-state" title="Send Message">
+                    <div class="send-svg-wrapper eval initial-state" title="Send Message">
                         <svg width="100%" height="100%" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M10.5004 12H5.00043M4.91577 12.2915L2.58085 19.2662C2.39742 19.8142 2.3057 20.0881 2.37152 20.2569C2.42868 20.4034 2.55144 20.5145 2.70292 20.5567C2.87736 20.6054 3.14083 20.4869 3.66776 20.2497L20.3792 12.7296C20.8936 12.4981 21.1507 12.3824 21.2302 12.2216C21.2993 12.082 21.2993 11.9181 21.2302 11.7784C21.1507 11.6177 20.8936 11.5019 20.3792 11.2705L3.66193 3.74776C3.13659 3.51135 2.87392 3.39315 2.69966 3.44164C2.54832 3.48375 2.42556 3.59454 2.36821 3.74078C2.30216 3.90917 2.3929 4.18255 2.57437 4.72931L4.91642 11.7856C4.94759 11.8795 4.96317 11.9264 4.96933 11.9744C4.97479 12.0171 4.97473 12.0602 4.96916 12.1028C4.96289 12.1508 4.94718 12.1977 4.91577 12.2915Z" stroke="#55b685" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                         </svg>
                     </div>
                     """
-                    upload_icon = gr.HTML(upload_html, elem_classes=["custom-upload"])
-                    send_button = gr.HTML(send_html, elem_classes=["custom-send"])
+                    upload_icon = gr.HTML(upload_html, elem_classes=["custom-upload", "eval"])
+                    send_button = gr.HTML(send_html, elem_classes=["custom-send", "eval-send"])
 
         # Chat History Sidebar
         with gr.Column(scale=2, elem_classes=["chat-history-sidebar"]):
@@ -183,7 +183,7 @@ def create_evaluate_interface():
 
                 # Initial state SVG HTML
                 initial_upload_html = """
-                <div class="upload-svg-wrapper initial-state" title="Upload Image" onclick="this.closest('.chat-controls').querySelector('input[type=\\'file\\']').click()">
+                <div class="upload-svg-wrapper eval initial-state" title="Upload Image" onclick="this.closest('.chat-controls').querySelector('input[type=\\'file\\']').click()">
                     <svg width="100%" height="100%" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M21 15V16.2C21 17.8802 21 18.7202 20.673 19.362C20.3854 19.9265 19.9265 20.3854 19.362 20.673C18.7202 21 17.8802 21 16.2 21H7.8C6.11984 21 5.27976 21 4.63803 20.673C4.07354 20.3854 3.6146 19.9265 3.32698 19.362C3 18.7202 3 17.8802 3 16.2V15M17 8L12 3M12 3L7 8M12 3V15" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                     </svg>
@@ -206,9 +206,9 @@ def create_evaluate_interface():
                     "You are a helpful AI assistant.",  # reset system prompt
                     gr.update(visible=False),  # hide left chatbot
                     gr.update(visible=False),  # hide right chatbot
-                    gr.update(elem_classes=["chat-input", "modern-input", "chat-input-initial"]),  # reset input style
+                    gr.update(elem_classes=["chat-input","eval-input", "modern-input", "chat-input-initial"]),  # reset input style
                     gr.update(value=initial_upload_html, elem_classes=["custom-upload"]),  # reset upload icon
-                    gr.update(value=initial_send_html, elem_classes=["custom-send"]),  # reset send button
+                    gr.update(value=initial_send_html, elem_classes=["custom-send", "eval-send"]),  # reset send button
                     gr.update(value=None, visible=False),  # clear image
                     *button_updates,  # update all buttons
                     gr.update(visible=False),  # hide Base markdown
@@ -222,7 +222,7 @@ def create_evaluate_interface():
                 session = sessions[idx]
                 
                 active_upload_html = """
-                <div class="upload-svg-wrapper active-chat force-active" title="Upload Image" onclick="this.closest('.chat-controls').querySelector('input[type=\\'file\\']').click()">
+                <div class="upload-svg-wrapper eval active-chat force-active" title="Upload Image" onclick="this.closest('.chat-controls').querySelector('input[type=\\'file\\']').click()">
                     <svg width="100%" height="100%" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M21 15V16.2C21 17.8802 21 18.7202 20.673 19.362C20.3854 19.9265 19.9265 20.3854 19.362 20.673C18.7202 21 17.8802 21 16.2 21H7.8C6.11984 21 5.27976 21 4.63803 20.673C4.07354 20.3854 3.6146 19.9265 3.32698 19.362C3 18.7202 3 17.8802 3 16.2V15M17 8L12 3M12 3L7 8M12 3V15" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                     </svg>
@@ -230,7 +230,7 @@ def create_evaluate_interface():
                 """
                 
                 active_send_html = """
-                <div class="send-svg-wrapper active-chat force-active" title="Send Message">
+                <div class="send-svg-wrapper eval active-chat force-active" title="Send Message">
                     <svg width="100%" height="100%" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M10.5004 12H5.00043M4.91577 12.2915L2.58085 19.2662C2.39742 19.8142 2.3057 20.0881 2.37152 20.2569C2.42868 20.4034 2.55144 20.5145 2.70292 20.5567C2.87736 20.6054 3.14083 20.4869 3.66776 20.2497L20.3792 12.7296C20.8936 12.4981 21.1507 12.3824 21.2302 12.2216C21.2993 12.082 21.2993 11.9181 21.2302 11.7784C21.1507 11.6177 20.8936 11.5019 20.3792 11.2705L3.66193 3.74776C3.13659 3.51135 2.87392 3.39315 2.69966 3.44164C2.54832 3.48375 2.42556 3.59454 2.36821 3.74078C2.30216 3.90917 2.3929 4.18255 2.57437 4.72931L4.91642 11.7856C4.94759 11.8795 4.96317 11.9264 4.96933 11.9744C4.97479 12.0171 4.97473 12.0602 4.96916 12.1028C4.96289 12.1508 4.94718 12.1977 4.91577 12.2915Z" stroke="#55b685" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                     </svg>
@@ -498,7 +498,7 @@ def create_evaluate_interface():
             
             if image is not None and is_initial_state:
                 return """
-                <div class="upload-svg-wrapper has-image initial-state" title="Image Attached" onclick="this.closest('.chat-controls').querySelector('input[type=\\'file\\']').click()">
+                <div class="upload-svg-wrapper eval has-image initial-state" title="Image Attached" onclick="this.closest('.chat-controls').querySelector('input[type=\\'file\\']').click()">
                     <svg width="100%" height="100%" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M16 5L18 7L22 3M12.5 3H7.8C6.11984 3 5.27976 3 4.63803 3.32698C4.07354 3.6146 3.6146 4.07354 3.32698 4.63803C3 5.27976 3 6.11984 3 7.8V16.2C3 17.8802 3 18.7202 3.32698 19.362C3.6146 19.9265 4.07354 20.3854 4.63803 20.673C5.27976 21 6.11984 21 7.8 21H17C17.93 21 18.395 21 18.7765 20.8978C19.8117 20.6204 20.6204 19.8117 20.8978 18.7765C21 18.395 21 17.93 21 17M10.5 8.5C10.5 9.60457 9.60457 10.5 8.5 10.5C7.39543 10.5 6.5 9.60457 6.5 8.5C6.5 7.39543 7.39543 6.5 8.5 6.5C9.60457 6.5 10.5 7.39543 10.5 8.5ZM14.99 11.9181L6.53115 19.608C6.05536 20.0406 5.81747 20.2568 5.79643 20.4442C5.77819 20.6066 5.84045 20.7676 5.96319 20.8755C6.10478 21 6.42628 21 7.06929 21H16.456C17.8951 21 18.6147 21 19.1799 20.7582C19.8894 20.4547 20.4547 19.8894 20.7582 19.1799C21 18.6147 21 17.8951 21 16.456C21 15.9717 21 15.7296 20.9471 15.5042C20.8805 15.2208 20.753 14.9554 20.5733 14.7264C20.4303 14.5442 20.2412 14.3929 19.8631 14.0905L17.0658 11.8527C16.6874 11.5499 16.4982 11.3985 16.2898 11.3451C16.1061 11.298 15.9129 11.3041 15.7325 11.3627C15.5279 11.4291 15.3486 11.5921 14.99 11.9181Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                     </svg>
@@ -536,15 +536,6 @@ def create_evaluate_interface():
                 new_size = (int(img.size[0]*ratio), int(img.size[1]*ratio))
                 return img.resize(new_size, Image.Resampling.LANCZOS)
             return img
-
-        def get_initial_svg_html():
-            return """
-            <div class="send-svg-wrapper" title="Send Message" style="width: 42px; height: 42px; transform: scale(1.2);">
-                <svg width="100%" height="100%" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M10.5004 12H5.00043M4.91577 12.2915L2.58085 19.2662C2.39742 19.8142 2.3057 20.0881 2.37152 20.2569C2.42868 20.4034 2.55144 20.5145 2.70292 20.5567C2.87736 20.6054 3.14083 20.4869 3.66776 20.2497L20.3792 12.7296C20.8936 12.4981 21.1507 12.3824 21.2302 12.2216C21.2993 12.082 21.2993 11.9181 21.2302 11.7784C21.1507 11.6177 20.8936 11.5019 20.3792 11.2705L3.66193 3.74776C3.13659 3.51135 2.87392 3.39315 2.69966 3.44164C2.54832 3.48375 2.42556 3.59454 2.36821 3.74078C2.30216 3.90917 2.3929 4.18255 2.57437 4.72931L4.91642 11.7856C4.94759 11.8795 4.96317 11.9264 4.96933 11.9744C4.97479 12.0171 4.97473 12.0602 4.96916 12.1028C4.96289 12.1508 4.94718 12.1977 4.91577 12.2915Z" stroke="#55b685" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                </svg>
-            </div>
-            """
 
         def get_active_svg_html():
             return """
